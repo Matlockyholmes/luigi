7596464882d3a4dbad99b6d2e88f1c45f52323fb
MHh10 <matthias.heylen@hi10.be>
2019-05-02 12:33:44 +0200

    index

--- src/main/java/be/vdab/luigi/controllers/IndexController.java
+++ src/main/java/be/vdab/luigi/controllers/IndexController.java
@@ -1,26 +1,16 @@
 package be.vdab.luigi.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalTime;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class IndexController {
     @GetMapping
     public String index(){
-        StringBuffer buffer = new StringBuffer("<!doctype html><html><title>Hallo</title><body>");
-        int uur = LocalTime.now().getHour();
-        if(uur < 12){
-            buffer.append("Goede morgen");
-        } else if (uur < 18){
-            buffer.append("Goede middag");
-        } else {
-            buffer.append("Goede avond");
-        }
-        buffer.append("</body><html>");
-        return buffer.toString();
+        return "index";
     }
 }
