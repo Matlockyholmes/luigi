7ac2dd06dd5cc5b97337edc6cf846ed9d70181b4
MHh10 <matthias.heylen@hi10.be>
2019-05-23 10:53:21 +0200

    index

--- src/main/java/be/vdab/luigi/services/DefaultEuroService.java
+++ src/main/java/be/vdab/luigi/services/DefaultEuroService.java
@@ -4,12 +4,10 @@ import be.vdab.luigi.exception.KoersClientException;
 import be.vdab.luigi.restclients.KoersClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.Arrays;
 
 @Service
 public class DefaultEuroService implements EuroService {
